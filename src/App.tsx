8c8f38b3794e27e89d1ca79680e2ee825154d7e4
ylhjxgogogo <1741754980@qq.com>
2024-06-17 20:54:04 +0800

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import "./App.css";
 const App = () => {
   return (
     <Grid
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch";
 import logo from "../assets/logo.webp";
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
-      <Text>vv</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
