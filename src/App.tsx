eeee802aa8f23752ffb46453fd52779d908afd4a
ylhjxgogogo <1741754980@qq.com>
2024-06-19 20:59:00 +0800

    fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show, HStack, VStack, Box } from "@chakra-ui/react";
+import { Grid, GridItem, Show, HStack, Box } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
